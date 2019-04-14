32fa0e0efe514caf8e3adea46aa0edcdb3107d6f
Jiri Erhart <jiri.erhart@strv.com>
2019-04-14 03:48:44 +0200

    Fix production settings

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,22 +1,11 @@
 'use strict'
 
-const fs = require('fs')
-
 module.exports = {
   hostname: 'https://maso23.herokuapp.com',
   database: {
     options: {
-      operatorsAliases: false,
-      dialectOptions: {
-        ssl: {
-          key: fs.readFileSync('/root/.postgresql/postgresql.key'),
-          cert: fs.readFileSync('/root/.postgresql/postgresql.crt'),
-          ca: fs.readFileSync('/root/.postgresql/root.crt'),
-        },
-      },
       logging: true,
     },
-    connectionString: process.env.DATABASE_URL
-      || 'postgresql://maso-web@10.10.11.11/maso?sslmode=verify-full',
+    connectionString: process.env.DATABASE_URL,
   },
 }
